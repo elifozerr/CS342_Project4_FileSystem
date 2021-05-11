f3cb661ce2ff743e15261d918b2396ebea150240
elifozerr <elifozerr1@outlook.com>
2021-05-11 12:24:51 +0300

    first commit

---
+++ simplefs.c
@@ -0,0 +1,42 @@
+#include "simplefs.h"
+
+int create_format_vdisk(char *vdiskname, int m){
+
+}
+
+int sfs_mount(char  *vdiskname){
+
+}
+
+int sfs_umount(){
+
+}
+
+int sfs_create(char  *filename){
+
+}
+
+int sfs_open(char *filename, int mode){
+
+}
+
+int sfs_getsize (int  fd){
+
+}
+
+int sfs_close(int  fd){
+
+}
+
+int sfs_read(int fd, void *buf, int n){
+
+}
+
+int sfs_append(int fd, void *buf, int n){
+
+}
+
+int sfs_delete(char  *filename){
+
+
+}
